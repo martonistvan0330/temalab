99d352d24a61b6a15750c148eeed35926e8ce791
martonistvan0330 <mistvan0330@gmail.com>
2021-11-08 23:13:49 +0100

    new auth_server start

---
+++ auth_server/src/main/java/hu/bme/aut/temalab/authserver/UserRepository.java
@@ -0,0 +1,8 @@
+package hu.bme.aut.temalab.authserver;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
